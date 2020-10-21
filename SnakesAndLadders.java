f07546982d1a8b781670af14c7eec810487e41a8
codexakshay <codexakshay@gmail.com>
2020-10-21 19:52:01 +0530

    UC1_DiceRoll : 1 to 6 Numbers

--- SnakesAndLadders.java
+++ SnakesAndLadders.java
@@ -5,10 +5,47 @@ import java.util.Random;
 public class SnakesAndLadders 
 {	
 	static int playerOne = 0;
+	private static Random rand = new Random();
+	public void diceRoll()
+	{
+		 	int roll = rand.nextInt(6) + 1;
+		 	System.out.println("Player One : " +playerOne);
+
+				 	if (playerOne  == 100)
+					 {
+						 System.exit(0);
+					 } 
+				 	
+					if (playerOne + roll > 100)
+					 {
+						roll = 0;
+						playerOne=playerOne + roll;
+					
+					 }
+					
+				 	if (playerOne < 100)
+				 	{
+				 		playerOne = playerOne + roll;
+				 		System.out.println("Player One Rolled : "+roll);
+				 	}
+
+ 	}
+
+	public void playGame()
+	{
+		while(true) 
+		{
+			diceRoll();
+		}
+		
+	}
+	
 	public static void main(String[] args) {
+		SnakesAndLadders object = new SnakesAndLadders();
+		object.playGame();
 	}
 
 }
 
 
-//UC1
+//UC2
